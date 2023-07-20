66e83be817d21cf521de50d95079ce0c5d49ebd3
nixiu <1940928852@qq.com>
2023-07-20 20:55:13 +0800

    add params

--- track_v1.py
+++ track_v1.py
@@ -3,6 +3,7 @@ import os
 import sys
 import numpy as np
 from detect.main_yolov5 import yolov5
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 mid_anchor_x = 240
@@ -12,7 +13,8 @@ parent_dir = os.path.dirname(current_dir)
 # Add the parent directory to Python's sys.path
 sys.path.append(parent_dir)
 
-#from control import send_to_arduino
+# from control import send_to_arduino
+from camera.preview import preview
 
 pre_anchor_midpoint_x = 0
 
@@ -45,9 +47,8 @@ def track():
     global pre_anchor_midpoint_x
     global video
     ret, frame = video.read()
-    #frame = cv2.rotate(frame, cv2.ROTATE_180)
 
-    #frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
+    frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
     if not ret:
         print('something went wrong')
         return
@@ -55,48 +56,59 @@ def track():
     ret, bbox = tracker.update(frame)
 
     now_anchor_midpoint_x = calculate_anchor_midpoint(bbox)
-    #move_motor_based_on_anchor_change(now_anchor_midpoint_x)
+    # move_motor_based_on_anchor_change(now_anchor_midpoint_x)
     pre_anchor_midpoint_x = now_anchor_midpoint_x
 
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-    print(fps)
-    # if ret:
-    #     p1 = (int(bbox[0]), int(bbox[1]))
-    #     p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    #     cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-    # else:
-    #     cv2.putText(frame, "Tracking failure detected", (100, 80),
-    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-    # cv2.putText(frame, "KCF Tracker", (100, 20),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-    # cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-    #
-    # # Display the tracking result
-    # cv2.imshow("Tracking", frame)
-    #
-    # # Exit if ESC key is pressed
-    # if cv2.waitKey(1) & 0xFF == 27:
-    #     exit()
+    print("FPS: ", int(fps))
+    if ret:
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+    else:
+        cv2.putText(frame, "Tracking failure detected", (100, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+    cv2.putText(frame, "KCF Tracker", (100, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+    preview(frame=frame)
 
-def detect_ini():#detect object to track and initialize the tracker
+
+def detect_ini():  # detect object to track and initialize the tracker
     while True:
         ret, frame = video.read()
+        frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
         if not ret:
             print('cannot read the video')
             return
-
+        cv2.imwrite('frame.jpg', frame)
         # 使用yolo模型进行检测
         dets = yolonet.detect(frame)
         boxes = yolonet.postprocess(frame, dets)
 
         # 如果检测到目标，使用第一个检测到的目标来初始化跟踪器
         if len(boxes) > 0:
-            tracker.init(frame, tuple(boxes[0]))
-            break
-        else:
-            print("No target detected. Retrying...")
+            # Get the first box
+            box = boxes[0]
+            x, y, w, h = box
+            area = w * h
 
+            # Maximum acceptable area
+            max_area = 10000
+
+            # If the area of the box is larger than the maximum acceptable area, scale it down
+            if area > max_area:
+                scale_factor = (max_area / area) ** 0.5  # square root to scale both width and height
+                w = int(w * scale_factor)
+                h = int(h * scale_factor)
+                # Recalculate the box
+                box = (x, y, w, h)
+
+            tracker.init(frame, tuple(box))
+            break
+
+        print("No target detected. Retrying...")
 
 
 
@@ -106,17 +118,20 @@ if __name__ == '__main__':
         print("Error: Unable to access the camera.")
     video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    # params = cv2.TrackerKCF_Params()
+    params = cv2.TrackerKCF_Params()
     # 设置参数
-    # params.detect_thresh = 0.70
-    # params.interp_factor = 0.02#
+    params.detect_thresh = 0.70
+    params.interp_factor = 0.2#
+    params.max_patch_size = 100*100
+    params.compress_feature = True
+    params.compressed_size = 1
     # 使用这些参数创建跟踪器
     tracker = cv2.TrackerKCF_create()
 
     ret, frame = video.read()
     frame_height, frame_width = frame.shape[:2]
-    yolonet = yolov5(yolo_type='yolov5s', confThreshold=0.90, nmsThreshold=0.5, objThreshold=0.5, path='./weights/')
-    #detect object to track and initialize the tracker
+    yolonet = yolov5(yolo_type='yolov5s', confThreshold=0.50, nmsThreshold=0.5, objThreshold=0.5, path='./weights/')
+    # detect object to track and initialize the tracker
     detect_ini()
     while True:
         track()
