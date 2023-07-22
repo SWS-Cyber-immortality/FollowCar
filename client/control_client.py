f7838ce34be914bb1a5bd8aa871fcae9c46831b9
xQAQyn <xqaqyn@gmail.com>
2023-07-22 13:50:01 +0800

    debug multi-thread

--- client/control_client.py
+++ client/control_client.py
@@ -3,6 +3,7 @@ import time
 import json
 import os
 import sys
+import queue
 import threading
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,10 +17,6 @@ sys.path.append(parent_dir)
 from control import send_to_arduino
 from client.track_engine import TrackEngine
 
-control_signal = None
-action_num = 20
-signal_valid = False
-tracking = False
 video = None
 tracker = None
 frame_height = None
@@ -30,10 +27,31 @@ mid_anchor_x = 160#mid point of anchor box
 lower_bound = 90 - max_angle
 upper_bound = 90 + max_angle
 
-arduino_command = None
-arduino_num = 0
+class ThreadSafeQueue:
+    def __init__(self):
+        self.data = queue.Queue()
+        self.lock = threading.Lock()
+
+    def add(self, item):
+        with self.lock:
+            self.data.put(item)
+
+    def get_last(self):
+        with self.lock:
+            item = (None, None)
+            while not self.data.empty():
+                item = self.data.get()
+            if item != (None, None):
+                self.data.put(item)
+            return item
+
+arduino_flow = ThreadSafeQueue()
+control_flow = ThreadSafeQueue()
+action_flow = ThreadSafeQueue()
+
 def arduino_control():
-    global arduino_command, arduino_num, tracking, signal_valid
+    tracking, signal_valid = control_flow.get_last()
+    arduino_command, arduino_num = arduino_flow.get_last()
     while True:
         if arduino_command is not None and (tracking is True or signal_valid is True):
             print('tracking: ', tracking, 'signal_valid: ', signal_valid, 'arduino_command: ', arduino_command, 'arduino_num: ', arduino_num)
@@ -43,6 +61,7 @@ def arduino_control():
                 send_to_arduino(arduino_command, str(arduino_num))
             if tracking is False and signal_valid is True and arduino_command != 'w':
                 signal_valid = False
+                control_flow.add((tracking, signal_valid))
             time.sleep(0.4)
 
 
@@ -54,7 +73,7 @@ def on_connect(client, userdata, flags, rc):
         print("Failed to connect. Error code: {}.".format(rc))
 
 def on_message(client, userdata, msg):
-    global tracking, video, tracker, frame_height, arduino_command, frame_width, signal_valid, signal_valid, control_signal, action_num
+    global video, tracker, frame_height, frame_width
     recv_file = msg.payload
     recv_dict = json.loads(recv_file)
     print(recv_dict)
@@ -67,35 +86,42 @@ def on_message(client, userdata, msg):
             print('track_prepare time: ', end_time - start_time)
             tracking = True
             signal_valid = False
+            control_flow.add((tracking,signal_valid))
         elif gesId == 23:  # Stop sign: stop follow, start to manual control
             control_signal='q'
             action_num = None
             tracking = False
             signal_valid = True
+            control_flow.add((tracking, signal_valid))
+            action_flow.add((control_signal, action_num))
         elif gesId == 16 or gesId == 18:  # Zoom in: Go ahead
             control_signal = 'w'
             action_num = 20
             signal_valid = True
+            tracking = False
+            control_flow.add((tracking, signal_valid))
+            action_flow.add((control_signal, action_num))
         elif gesId == 17 or gesId == 19:  # Zoom out: Go back
             control_signal = 's'
             action_num = 20
             signal_valid = True
+            tracking = False
+            control_flow.add((tracking, signal_valid))
+            action_flow.add((control_signal, action_num))
         elif gesId == 0 or gesId == 6:  # Swiping left: turn left
             control_signal = 'h'
             action_num = 60
             signal_valid = True
+            tracking = False
+            control_flow.add((tracking, signal_valid))
+            action_flow.add((control_signal, action_num))
         elif gesId == 1 or gesId == 7:  # Swiping right: turn right
             control_signal = 'g'
             action_num = 60
             signal_valid = True
-        elif gesId == 18 and control_signal == 'w':
-            control_signal = 'p'
-            action_num = None
-            signal_valid = True
-        elif gesId == 19 and control_signal == 'w':
-            control_signal = 'o'
-            action_num = None
-            signal_valid = True
+            tracking = False
+            control_flow.add((tracking, signal_valid))
+            action_flow.add((control_signal, action_num))
         elif gesId == 24:  # purchase apple
             start_time = time.perf_counter()
             video, tracker, frame_height, frame_width = track_engine.track_prepare(47)
@@ -103,6 +129,7 @@ def on_message(client, userdata, msg):
             print('track_prepare time: ', end_time - start_time)
             tracking = True
             signal_valid = False
+            control_flow.add((tracking, signal_valid))
 
 def setup(hostname):
     client = mqtt.Client()
@@ -115,18 +142,14 @@ def setup(hostname):
 previous_angle = None
 
 def move_motor_based_on_anchor_change(now_anchor_midpoint_x, threshold=50):
-    global previous_angle, arduino_command, arduino_num
+    global previous_angle
     movement_threshold = threshold  # Set the threshold for motor movement
     gap_X = mid_anchor_x - now_anchor_midpoint_x  # Change the order to invert the direction
 
     # Map the x-coordinate of the target (now_anchor_midpoint_x) from the image coordinate system (0 to 320)
     # to the servo coordinate system (lower_bound to upper_bound)
     angle = -0.1875*now_anchor_midpoint_x + 120
-    # angle = upper_bound + ((now_anchor_midpoint_x / 320) * (upper_bound - lower_bound))
-    
-    # print("angle: ", angle)
-    # print("previous_angle: ", previous_angle)
-    # print("x: ", now_anchor_midpoint_x)
+
     # Only send the new angle to the Arduino if the difference with the previous angle is larger than 10 degrees
     if previous_angle is None or abs(previous_angle - angle) > 2:
         send_to_arduino('r', str(int(angle)))  # Move the servo
@@ -136,12 +159,15 @@ def move_motor_based_on_anchor_change(now_anchor_midpoint_x, threshold=50):
         if now_anchor_midpoint_x > mid_anchor_x:
             arduino_command = 'd'
             arduino_num = 20
+            arduino_flow.add((arduino_command, arduino_num))
         else:
             arduino_command = 'a'
             arduino_num = 20
+            arduino_flow.add((arduino_command, arduino_num))
     else:
         arduino_command = 'w'
         arduino_num = 10
+        arduino_flow.add((arduino_command, arduino_num))
 
 
 if __name__ == '__main__':
@@ -154,12 +180,13 @@ if __name__ == '__main__':
     arduino_signal_thread.start()
 
     while True:
+        tracking, signal_valid = control_flow.get_last()
         if tracking is True:
             now_anchor_midpoint_x = track_engine.track(tracker, video, frame_height, frame_width)
             move_motor_based_on_anchor_change(now_anchor_midpoint_x)
         elif signal_valid is True:
-            arduino_command = control_signal
-            arduino_num = action_num
+            control_signal, action_num = action_flow.get_last()
+            arduino_flow.add((control_signal, action_num))
             track_engine.preview_from_camera()
         else:
             track_engine.preview_from_camera()
